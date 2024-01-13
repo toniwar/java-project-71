fe52c797451366421ead6875e5a603a1eccb0d45
Tonius Var <toniuswar@gmail.com>
2024-01-13 23:27:35 +0300

    implemented Picocli

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,16 @@
+
 plugins {
     application
+    id("checkstyle")
 }
 
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+tasks.withType<JavaCompile> {
+    options.release = 20
+}
 group = "io.hexlet"
 version = "1.0-SNAPSHOT"
 
@@ -12,8 +21,14 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // https://mvnrepository.com/artifact/info.picocli/picocli
+    implementation("info.picocli:picocli:4.7.5")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.5")
+
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,28 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Command;
+
+@Command(name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference."
+)
+public class App implements Runnable {
+    @Option(names = { "-h", "--help" }, usageHelp = true, description = "Show this help message and exit.")
+    private Boolean help = false;
+
+    @Option(names = { "-V", "--version" }, description = "Print version information and exit.")
+    private Boolean version = false;
+
+
     public static void main(String[] args) {
-        System.out.println("Hello, Hexlet!");
+        new CommandLine(new App()).execute(args);
+    }
+
+    @Override
+    public void run() {
+
     }
 }
